a57e61c732ea9c90184a5796fdab8353b957b4b8
chiahsoon <chiahsoon18@gmail.com>
2022-01-28 15:55:46 +0800

    fix: use Text(En/De)coder instead of old solution

--- src/utils/fileHelper.ts
+++ src/utils/fileHelper.ts
@@ -1,13 +1,10 @@
-// src: https://developers.google.com/web/updates/2012/06/How-to-convert-ArrayBuffer-to-and-from-String
-export const ab2str = (buf: ArrayBuffer) => {
-    return String.fromCharCode.apply(null, Array.from(new Uint16Array(buf)));
+// https://developers.google.com/web/updates/2014/08/Easier-ArrayBuffer-String-conversion-with-the-Encoding-API
+export const ab2str = (buf: ArrayBuffer): string => {
+    const decoder = new TextDecoder();
+    return decoder.decode(buf);
 };
 
-export const str2ab = (str: string) => {
-    const buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
-    const bufView = new Uint16Array(buf);
-    for (let i=0, strLen=str.length; i < strLen; i++) {
-        bufView[i] = str.charCodeAt(i);
-    }
-    return buf;
+export const str2ab = (str: string): ArrayBuffer => {
+    const encoder = new TextEncoder();
+    return encoder.encode(str).buffer;
 };

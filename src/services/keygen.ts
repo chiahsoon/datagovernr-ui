02f3d7bfa6e7f1022e8ecea5b0ae91cca8766477
Chua Chiah Soon <chiahsoon18@gmail.com>
2022-02-11 15:42:06 +0800

    fix: merge conflicts

--- src/services/keygen.ts
+++ src/services/keygen.ts
@@ -1,31 +1,27 @@
 import forge from 'node-forge';
+import {SALT_LENGTH, generateKey} from './keygen/pbkdf2';
 import {FileEncryptionScheme, FileEncryptionService} from './encryption';
 
-// TODO: Refactor properly
 export const encryptWithPassword = (dataBinaryBuf: ArrayBuffer, password: string): [string, string] => {
-    // 20-byte salt to match output length of PBKDF2 hash function (default SHA-1)
-    const saltBinary = forge.random.getBytesSync(20);
+    // dataBinaryBuf is a buffer that stores the data in binary format
+    const saltBinary = forge.random.getBytesSync(SALT_LENGTH);
     const saltBase64 = forge.util.encode64(saltBinary);
-    const keyBinary = generateKey(password, saltBinary);
-    const cipher = FileEncryptionService.createEncryptionInstance(FileEncryptionScheme.AES256GCM, keyBinary);
+    const keyBinary = generateKey(password, saltBinary,
+        FileEncryptionService.getKeyLength(FileEncryptionScheme.AES256GCM));
+    const cipher = FileEncryptionService.createEncryptionInstance(
+        FileEncryptionScheme.AES256GCM,
+        keyBinary);
     const encryptedBinaryString = cipher.encryptFile(dataBinaryBuf);
     return [encryptedBinaryString, saltBase64];
 };
 
 export const decryptWithPassword = (dataBinaryBuf: ArrayBuffer, password: string, saltBase64: string): string => {
-    // dataBinaryBuf is a buffer that stores the data in binary format
     // saltBase64 is a string that stores the salt in base64 format
     const saltBinary = forge.util.decode64(saltBase64);
-    const keyBinary = generateKey(password, saltBinary);
+    const keyBinary = generateKey(password, saltBinary,
+        FileEncryptionService.getKeyLength(FileEncryptionScheme.AES256GCM));
     const decipher = FileEncryptionService.createEncryptionInstance(
         FileEncryptionScheme.AES256GCM,
         keyBinary);
     return decipher.decryptFile(dataBinaryBuf);
-};
-
-export const generateKey = (password: string, salt: string): string => {
-    // Extract password derivation into new module
-    const keySizeBytes = FileEncryptionService.getKeyLength(FileEncryptionScheme.AES256GCM);
-    const numIterations = 1000;
-    return forge.pkcs5.pbkdf2(password, salt, numIterations, keySizeBytes);
 };

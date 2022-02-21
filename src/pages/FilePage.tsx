df285e2925ec03931a16bda01d4134e69a89678c
Chua Chiah Soon <chiahsoon18@gmail.com>
2022-02-21 16:30:27 +0800

    feat: display verification link properly

--- src/pages/FilePage.tsx
+++ src/pages/FilePage.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {Button, Col, Descriptions, List, Row, Tooltip, Typography} from 'antd';
 import {ErrorPage} from './ErrorPage';
-import {EmptyVerificationDetails, isNotVerified} from '../types/verificationDetails';
+import {EmptyVerificationDetails, isNotVerified, VerificationDetails} from '../types/verificationDetails';
 import {getFileVerificationDetails} from '../web/api';
 import {displayError} from '../utils/error';
 import MainLayout from './MainLayout';
@@ -43,6 +43,15 @@ export const FilePage = () => {
         return '';
     };
 
+    const getLink = (details: VerificationDetails): string | undefined => {
+        const link = details.verifier.link;
+        const isApiLinkPrefix = 'api://';
+        const apiUrl = process.env.REACT_APP_API_URL || '';
+        if (isNotVerified(details)) return undefined;
+        if (link.startsWith(isApiLinkPrefix)) return link.replace(isApiLinkPrefix, apiUrl + '/');
+        return link;
+    };
+
     return (
         <MainLayout name={'DataGovernR'}>
             <Row gutter={[16, 16]}>
@@ -59,18 +68,11 @@ export const FilePage = () => {
                         <Descriptions.Item label="File ID" span={3}>{fileId}</Descriptions.Item>
                         <Descriptions.Item label="Verification Link" span={3}>
                             {
-                                isNotVerified(verificationDetails) ?
+                                getLink(verificationDetails) == null ?
                                     <Text italic>File not verified</Text> :
-                                    <Link href={verificationDetails.verifier.link} target="_blank">
-                                        {verificationDetails.verifier.link}
+                                    <Link href={getLink(verificationDetails)} target="_blank">
+                                        {getLink(verificationDetails)}
                                     </Link>
-                            }
-                        </Descriptions.Item>
-                        <Descriptions.Item label="Verified At" span={3}>
-                            {
-                                isNotVerified(verificationDetails) ?
-                                    '-' :
-                                    verificationDetails.verifier.createdDate.toDateString()
                             }
                         </Descriptions.Item>
                     </Descriptions>

4c5a673b438976e5bf7b9878911bbaa21be87d7b
Gu Wangfan <wangfan_gu.14@u.nus.edu>
2022-02-28 15:57:38 +0800

    Add support for splitting the key into more than shares
    (decryption/rebuilding key from shares already supports this)

--- src/services/keysplit.ts
+++ src/services/keysplit.ts
@@ -1,16 +1,41 @@
 import forge from 'node-forge';
 
-// Extension - support for splitting into more than 2 shares?
-export const splitKey = (keyBinary: string): string[] => {
+/* Splits the key k into n shares:
+ *   k^x_0, x_0^x_1, ... x_n
+ * for independent random x_i
+ */
+export const splitKey = (keyBinary: string, numShares: number =2): string[] => {
+    /*
     const otpBinary = forge.random.getBytesSync(keyBinary.length);
     let cipherBinary = '';
     for (let i = 0; i < keyBinary.length; i++) {
         cipherBinary += String.fromCharCode(keyBinary.charCodeAt(i) ^ otpBinary.charCodeAt(i));
     }
     return [otpBinary, cipherBinary];
+    */
+    if (!Number.isInteger(numShares) || numShares < 2) {
+        throw new RangeError("Invalid parameter.");
+    }
+    let tempBinary = keyBinary.slice();
+    let randBinary = '';
+    let shareArray = [];
+    for (let j = 1; j < numShares; j++) {
+        randBinary = forge.random.getBytesSync(keyBinary.length);
+        let cipherBinary = '';
+        for (let i = 0; i < tempBinary.length; i++) {
+            cipherBinary += String.fromCharCode(tempBinary.charCodeAt(i) ^ randBinary.charCodeAt(i));
+        }
+        shareArray.push(cipherBinary);
+        tempBinary = randBinary.slice();
+    }
+    shareArray.push(randBinary);
+    return shareArray;
 };
 
 export const rebuildKey = (shareBinaryArr: string[]): string => {
+    if (shareBinaryArr.length < 2) {
+        throw new RangeError("Not enough shares.");
+    }
     let keyBinary = shareBinaryArr[0].slice();
     let tempBinary = '';
     for (let j = 1; j < shareBinaryArr.length; j++) {

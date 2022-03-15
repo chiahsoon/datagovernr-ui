ccc22febcdc2d490a5542e5f898dfaec08c958a0
Gu Wangfan <wangfan.gu@ntu.edu.sg>
2022-03-15 14:48:44 +0800

    Update PBKDF2 iteration count

--- src/services/keygen/pbkdf2.ts
+++ src/services/keygen/pbkdf2.ts
@@ -1,7 +1,7 @@
 import forge from 'node-forge';
 
 export const SALT_LENGTH = 64;
-const NUM_ITERATIONS = 10000;
+const NUM_ITERATIONS = 120000; // OWASP recommended count for SHA512
 
 // Key Derivation using PBKDF2-SHA512
 export function generateKey(password: string, salt: string, keyLength: number): string {
